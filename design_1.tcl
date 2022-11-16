cfe09a8db87870fa891ed33e7bdd5df71047f068
ESCristiano <cris96r@gmail.com>
2022-11-16 19:16:36 +0000

    Update design.tcl

--- design_1.tcl
+++ design_1.tcl
@@ -1139,4 +1139,38 @@ common::send_gid_msg -ssname BD::TCL -id 2052 -severity "CRITICAL WARNING" "This
 
 create_root_design ""
 
+##################################################################
+# Wrap Design 
+##################################################################
+make_wrapper -files [get_files $script_folder/backstabbing_devil/project_1.srcs/sources_1/bd/design_1/design_1.bd] -top
+add_files -norecurse $script_folder/backstabbing_devil/project_1.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
 
+##################################################################
+# Generate Output Products
+##################################################################
+generate_target all [get_files $script_folder/backstabbing_devil/project_1.srcs/sources_1/bd/design_1/design_1.bd]
+catch { config_ip_cache -export [get_ips -all design_1_AXI_PerfectTranslator_0_0] }
+catch { config_ip_cache -export [get_ips -all design_1_backstabber_0_0] }
+catch { config_ip_cache -export [get_ips -all design_1_byte_writer_0_0] }
+catch { config_ip_cache -export [get_ips -all design_1_rst_ps8_0_99M_0] }
+catch { config_ip_cache -export [get_ips -all design_1_smartconnect_0_0] }
+catch { config_ip_cache -export [get_ips -all design_1_system_ila_0_0] }
+catch { config_ip_cache -export [get_ips -all design_1_vio_0_0] }
+catch { config_ip_cache -export [get_ips -all design_1_zynq_ultra_ps_e_0_0] }
+export_ip_user_files -of_objects [get_files $script_folder/backstabbing_devil/project_1.srcs/sources_1/bd/design_1/design_1.bd] -no_script -sync -force -quiet
+create_ip_run [get_files -of_objects [get_fileset sources_1] $script_folder/backstabbing_devil/project_1.srcs/sources_1/bd/design_1/design_1.bd]
+launch_runs design_1_AXI_PerfectTranslator_0_0_synth_1 design_1_backstabber_0_0_synth_1 design_1_byte_writer_0_0_synth_1 design_1_rst_ps8_0_99M_0_synth_1 design_1_smartconnect_0_0_synth_1 design_1_system_ila_0_0_synth_1 design_1_vio_0_0_synth_1 design_1_zynq_ultra_ps_e_0_0_synth_1 -jobs 8
+export_simulation -of_objects [get_files $script_folder/backstabbing_devil/project_1.srcs/sources_1/bd/design_1/design_1.bd] -directory $script_folder/backstabbing_devil/project_1.ip_user_files/sim_scripts -ip_user_files_dir $script_folder/backstabbing_devil/project_1.ip_user_files -ipstatic_source_dir $script_folder/backstabbing_devil/project_1.ip_user_files/ipstatic -lib_map_path [list {modelsim=$script_folder/backstabbing_devil/project_1.cache/compile_simlib/modelsim} {questa=$script_folder/backstabbing_devil/project_1.cache/compile_simlib/questa} {xcelium=$script_folder/backstabbing_devil/project_1.cache/compile_simlib/xcelium} {vcs=$script_folder/backstabbing_devil/project_1.cache/compile_simlib/vcs} {riviera=$script_folder/backstabbing_devil/project_1.cache/compile_simlib/riviera}] -use_ip_compiled_libs -force -quiet
+
+##################################################################
+# Generate Bitstream
+##################################################################
+reset_run design_1_AXI_PerfectTranslator_0_0_synth_1
+reset_run design_1_backstabber_0_0_synth_1
+reset_run design_1_byte_writer_0_0_synth_1
+reset_run design_1_rst_ps8_0_99M_0_synth_1
+reset_run design_1_smartconnect_0_0_synth_1
+reset_run design_1_system_ila_0_0_synth_1
+reset_run design_1_vio_0_0_synth_1
+reset_run design_1_zynq_ultra_ps_e_0_0_synth_1
+launch_runs impl_1 -to_step write_bitstream -jobs 8
