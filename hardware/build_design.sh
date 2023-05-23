dd8332409a39e8a95f370d63cc41a13b15a25394
Diogo21Costa <53279352+Diogo21Costa@users.noreply.github.com>
2023-05-23 16:13:30 +0100

    fix(script): Avoid script error if backstabbing_devil does not exist

--- hardware/build_design.sh
+++ hardware/build_design.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-rm -r backstabbing_devil
+
+if [ -d ./backstabbing_devil ]
+    then  
+        echo $'\e[1;31m Deleting backstabbing_devil directory...\e[0m'
+        rm -r backstabbing_devil
+fi
+
+echo $'\e[1;31m Building hardware components...\e[0m'
 # vivado -source design_1.tcl
 vivado -mode tcl -source design_1.tcl
