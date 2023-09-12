34d499a947abd205bb54ceeb4d0edfbe8328bed3
ESCristiano <cris96r@gmail.com>
2023-09-12 09:15:24 +0100

    feat(zcu104): Add support for ZCU104, vivado
    Signed-off-by: ESCristiano <cris96r@gmail.com>

--- hardware/devil-in-fpga/build.sh
+++ hardware/devil-in-fpga/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if the script has at least 2 arguments
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 [graph|build] [zcu102|zcu104]"
+    exit 1
+fi
+
 if [ -d ./devil-in-fpga ]
     then  
         echo $'\e[1;31m Deleting devil-in-fpga directory...\e[0m'
@@ -9,28 +15,39 @@ fi
 rm *.jou *.log
 
 # Open project in graphical mode
-if [ "$1" == "graph" ]; then
-    if [ "$2" == "zcu102" ]; then
-        vivado -source build_graph_zcu102.tcl
-    fi 
-    if [ "$2" == "zcu104" ]; then
-        vivado -source build_graph_zcu104.tcl
-    fi 
-    if [ "$2" != "zcu102" ]  && [ "$2" != "zcu104" ]
-    then
-        echo $'\e[1;31m Board not Supported.\e[0m Use zcu102 or zcu104'
-    fi 
-else
-    if [ "$2" == "zcu102" ]; then
-        echo $'\e[1;31m Building hardware components ZCU102...\e[0m'
-        vivado -source build_zcu102.tcl
-    fi 
-    if [ "$2" == "zcu104" ]; then
-        echo $'\e[1;31m Board not Supported.\e[0m Use zcu102 or zcu104'
-        vivado -source build_zcu104.tcl
-    fi 
-    if [ "$2" != "zcu102" ]  && [ "$2" != "zcu104" ]
-    then
-        echo $'\e[1;31m Board not Supported.\e[0m Use zcu102 or zcu104'
-    fi 
-fi
+case "$1" in
+    "graph")
+        case "$2" in
+            "zcu102")
+                echo $'\e[1;31m Building hardware components ZCU102 in graphical mode...\e[0m'
+                vivado -source build_graph_zcu102.tcl
+                ;;
+            "zcu104")
+                echo $'\e[1;31m Building hardware components ZCU104 in graphical mode...\e[0m'
+                vivado -source build_graph_zcu104.tcl
+                ;;
+            *)
+                echo $'\e[1;31m Board not Supported in graphical mode.\e[0m Use zcu102 or zcu104'
+                ;;
+        esac
+        ;;
+    "build")
+        case "$2" in
+            "zcu102")
+                echo $'\e[1;31m Building hardware components ZCU102...\e[0m'
+                vivado -source build_zcu102.tcl
+                ;;
+            "zcu104")
+                echo $'\e[1;31m Building hardware components ZCU104...\e[0m'
+                vivado -source build_zcu104.tcl
+                ;;
+            *)
+                echo $'\e[1;31m Board not Supported in build mode.\e[0m Use zcu102 or zcu104'
+                ;;
+        esac
+        ;;
+    *)
+        echo "Usage: $0 [graph|build] [zcu102|zcu104]"
+        exit 1
+        ;;
+esac
