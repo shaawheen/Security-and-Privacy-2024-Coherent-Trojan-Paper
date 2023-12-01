e8233036a7e91c83539b936b1d38992901e1a266
ESCristiano <cris96r@gmail.com>
2023-12-01 18:53:15 -0500

    feat: Update rggen script to automatically copy srcs to the IPs
    Signed-off-by: ESCristiano <cris96r@gmail.com>

--- hardware/devil-in-fpga/rggen/1_generate.sh
+++ hardware/devil-in-fpga/rggen/1_generate.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+CLEAR='\e[0m'
+RED='\e[1;31m'
+GREEN='\e[1;32m'
+
 if [ -d ./out ]
-    then  
-        echo $'\e[1;31m Deleting out directory...\e[0m'
+    then 
+        echo -e $''${RED}' Deleting out directory... '${CLEAR}'' 
         rm -r out
 fi
 
 rggen -c config.yml devil_reg_file.yml -o out --plugin rggen-verilog
+
+IP=backstabber_1.0
+cp ./out/devil_register_file.v ../../ip_repo/$IP/src
+cp ./out/devil_register_file.vh ../../ip_repo/$IP/src
+echo -e $''${GREEN}' New register file copied to '$IP' '${CLEAR}''
+
+IP=test_register_file_1.0
+cp ./out/devil_register_file.v ../../ip_repo/$IP/src
+cp ./out/devil_register_file.vh ../../ip_repo/$IP/src
+echo -e $''${GREEN}' New register file copied to '$IP' '${CLEAR}''
+
