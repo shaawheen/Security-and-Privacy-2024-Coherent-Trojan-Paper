065c02a1667faae2035777635eddf4b00e2c6e3f
ESCristiano <cris96r@gmail.com>
2024-03-19 13:29:14 +0700

    feat(RSA_attack): Add results and script to reproduce the results
    Signed-off-by: ESCristiano <cris96r@gmail.com>

---
+++ results/Confidentiality/002_RSA_KEY_LEAK_POC_mar_19/send.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+
+ROOT_DIR=$(realpath "../../../")
+ARTIFACTS_DIR=${ROOT_DIR}/software/artifacts
+LINUX_APPS_DIR=${ARTIFACTS_DIR}/linux_apps
+OPENSSL_DIR=${LINUX_APPS_DIR}/openssl-install
+FIND_PATTERN_DIR=${LINUX_APPS_DIR}/find_pattern
+READ_BRAM_DIR=${LINUX_APPS_DIR}/read_BRAM
+
+ssh-keygen -f "/home/${USER}/.ssh/known_hosts" -R "192.168.42.15"
+
+# Send artifacts to the target board
+echo "--------------------------------------"
+echo "Sending artifacts to the target board"
+echo "--------------------------------------"
+
+scp -r rsa_attack.sh \
+    ${READ_BRAM_DIR}/read_BRAM \
+    ${OPENSSL_DIR}/openssl.sh \
+    ${OPENSSL_DIR}/bin/openssl \
+    ${OPENSSL_DIR}/libssl.so.3 \
+    ${OPENSSL_DIR}/libcrypto.so.3 \
+    ${FIND_PATTERN_DIR}/find_pattern \
+    root@192.168.42.15:
